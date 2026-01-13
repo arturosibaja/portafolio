0440420d38aeeaf73660803180e8d5cf513b2b71
arturosibaja <madangasoul@gmail.com>
2026-01-13 08:31:16 -0600

    Update pricing_analysis.sql

--- pricing_analysis.sql
+++ pricing_analysis.sql
@@ -8,13 +8,13 @@ SELECT
     ROUND(((price_usd - cost_usd)/price_usd)*100,2) as pct_margin, -- Margin formula = (price-cost) / price
     (
     SELECT ROUND(AVG(price_usd),2)               -- Subquery webcam avg price
-    FROM products as avg_price
+    FROM products
 	WHERE name like "%webcam%"
     ) AS avg_webcam_price,
 	(
     SELECT  ROUND(AVG(((price_usd - cost_usd)/price_usd)*100),2)  -- Subquery webcam avg margin
-    FROM products as avg_margin_pct
+    FROM products
 	WHERE name like "%webcam%"
-    ) AS avg_webcam_price	
+    ) AS avg_webcam_margin	
 FROM products
 WHERE name LIKE "%webcam%";
